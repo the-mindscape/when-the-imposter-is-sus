d3ce4a8982b1d985cca2e5fc063afc064d836db5
Mindscape <mindscape.gg@gmail.com>
2024-08-05 00:33:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from keep_alive import keep_alive
 
 status = "dnd" #online/dnd/idle
 
-custom_status = "" #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
+custom_status = "Your place, Our home." #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
 
 usertoken = os.getenv("TOKEN")
 if not usertoken:
